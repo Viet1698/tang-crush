92e6398751a16885db369258d779d2e5b2001b20
Viet1698 <87063461+Viet1698@users.noreply.github.com>
2021-07-07 16:24:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Em à!',
     introDesc: `Em có phải là nhà ảo thuật k???
-    Tại sao mỗi khi anh nhìn em
-    Mọi thứ xung quanh đều biến mất
-    Chỉ còn mỗi e trong mắt của anh thôi`,
+    Tại sao mỗi khi anh nhìn em.
+    Mọi thứ xung quanh đều biến mất.
+    Chỉ còn nhìn thấy mỗi em`,
     btnIntro: 'Ahihi',
     title: '  😙',
     desc: 'Trả lời thật nha, Em thích anh từ khi nào ',
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không hề nha, Anh trai à :3',
     question:'Nói cho anh biết lý do em thích anh đi <333',
     btnReply: 'Gửi cho anh <3',
-    reply: ' ',
-    mess: 'Anh biết mà 🥰.  😘😘',
+    reply: ' Không được viết. Anh muốn e gọi nói cho anh nghe cơ <3',
+    mess: 'Hihi.  🥰.  😘😘',
     messDesc: 'Yêu em. Công chúa của anh.',
-    btnAccept: 'Ahihi <3',
+    btnAccept: 'I love you 3000 <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
